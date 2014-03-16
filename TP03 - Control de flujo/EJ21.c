f3eb58654c6a046d136a76d6e9412fa57a941951
jpmrno <jpmrno@gmail.com>
2014-03-16 13:10:11 -0300

    int EJ21

---
+++ TP03 - Control de flujo/EJ21.c
@@ -0,0 +1,23 @@
+//
+//  Ej 21.c
+//  Programacion Imperativa
+//
+//  Created by Juan Moreno on 3/10/14.
+//  Copyright (c) 2014 Juan Moreno. All rights reserved.
+//
+
+/* Ejercicio 21
+Escribir un programa que lea un número entero, y escriba por salida estándar 
+dicho número y un cartel indicando si es capicúa o no. (En no más de 10 líneas)
+*/
+
+#include <stdio.h>
+#include "getnum.h"
+
+int main(void) {
+	int numero;
+	printf("Introduce un numero entero: ");
+	while(((numero = getchar()) >= '0' && numero <= '9')){
+
+	}
+}
